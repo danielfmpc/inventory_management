06567833631500d4612b1a7837198fde4be60f7d
Daniel Fernando <daniel_fmpc@hotmail.com.br>
2020-10-09 10:19:54 -0300

    componentização

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,3 +1,8 @@
-import { soma } from './soma';
+import React from "react";
+import { render } from "react-dom";
+import App from "./app";
 
-console.log(soma(1, 5));
+render(
+  <App />,
+  document.getElementById('app')
+);
